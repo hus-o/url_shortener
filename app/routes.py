48b5d68c2aa2b8f51ba88b780c4596755766f980
huseyin <thehusko@gmail.com>
2020-06-03 11:57:42 +0100

    working shortener and redirect

--- app/routes.py
+++ app/routes.py
@@ -17,28 +17,37 @@ def submitUrl():
         returned_URL= db.session.query(URL).filter(URL.long_URL==longURL).first()
         #check if long url in db
         if returned_URL == None:
-            '''
-            rationale:
-                -   strings are added to the database in the order that they come from respective clients
-                -   the string is first added to the db and then the id is reteirved to make an short url from it using the base62 module
-                    it is done this way because:
-                    -   we want to make sure that there are no race conditions; each url must be assigned a   
-            '''            
-            BIG_NUMBER=100000000000000 # to make the urls longer
+                 
+            # BIG_NUMBER=100000000000000 # to make the urls longer
 
             URL_object= URL(long_URL=longURL) # add longURL to database
             db.session.add(URL_object) # add to session 
             db.session.commit() # commit to database
     
-            short_URL=base62.encode(URL_object.id+BIG_NUMBER) # create short url
+            short_URL=base62.encode(URL_object.id) # create short url
             
             URL_object.short_URL=short_URL # update the recently created url object so that it now has a shortUrl
             db.session.commit() # commit to teh url database
+        
+            short_URL= URL_object.short_URL
 
-            print("new short url", URL_object.short_URL)        
-        
         else:
-            print("existing short url:", returned_URL.short_URL)
-        
-        return redirect(url_for('index'))
+            short_URL=returned_URL.short_URL
+
+
+        return render_template("index.html", shortURL = f"http://localhost:5000/{short_URL}", originalURL = longURL)
+
+@app.route("/<short_URL_client>", methods=["GET"])
+def shortAPI(short_URL_client):
+    og_URL = db.session.query(URL).filter(URL.short_URL==short_URL_client).first()
+
+    if og_URL is None:
+
+        return render_template("error.html")
+    
+    else:
+        return redirect(f"https://{og_URL.long_URL}")
+
+
+
         
